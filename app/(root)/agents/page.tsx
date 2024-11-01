5632995e7b3cbeae879cc1e9778b5f341e95ee9f
kelvinguchu <kelvinguchu5@gmail.com>
2024-11-01 13:51:18 +0300

    route grouping

--- app/(root)/agents/page.tsx
+++ app/(root)/agents/page.tsx
@@ -2,7 +2,7 @@ import Agents from "@/components/Agents";
 import localFont from "next/font/local";
 
 const geistMono = localFont({
-  src: "../../public/fonts/GeistMonoVF.woff",
+  src: "../../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
--- app/(root)/reports/page.tsx
+++ app/(root)/reports/page.tsx
@@ -4,7 +4,7 @@ import Reports from "@/components/Reports";
 import localFont from "next/font/local";
 
 const geistMono = localFont({
-  src: "../../public/fonts/GeistMonoVF.woff",
+  src: "../../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
