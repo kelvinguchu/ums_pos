f22d5f03c930f15d21fbe7f7bcb9d9aa26aa391c
kelvinguchu <kelvinguchu5@gmail.com>
2024-11-11 21:49:37 +0300

    css fix

--- components/sales/MeterSalesRow.tsx
+++ components/sales/MeterSalesRow.tsx
@@ -153,7 +153,7 @@ export function MeterSalesRow({
           </div>
 
           {/* Search Input */}
-          <div className='relative sticky top-0 bg-white z-10 pb-4'>
+          <div className='sticky top-0 bg-white z-10 pb-4'>
             <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground' />
             <Input
               className='pl-9'
